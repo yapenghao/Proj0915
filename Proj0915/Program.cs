694ca9599e74e0aca23b73ffe3078010f6a19a26
yapeng hao <yapeng.hao@email.com>
2019-09-15 12:37:58 -0400

    first commit

---
+++ Proj0915/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Proj0915
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world 0915");
+            Thread.Sleep(3000);
+        }
+    }
+}
